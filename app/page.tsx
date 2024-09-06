9bad2de7a04248752071996c7d8f7170b39748cb
sudeera-madushan <121970327+sudeera-madushan@users.noreply.github.com>
2024-09-06 02:00:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+import PostsPage from "./posts/page";
+import Header from "@/app/components/layout/header";
+import Test from "./posts/Test";
+
+export default function Home(): JSX.Element {
+  return <Test />;
+}
